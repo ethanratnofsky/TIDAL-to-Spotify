ba0f631167ca07096ae950d35f292b0c14f30799
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-09-13 09:31:43 -0400

    created function to print horizontal divider in output

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import tidalapi
 from dotenv import load_dotenv
 
 
+def print_hdiv():
+    print('----------')
+
+
 def main():
     # Loads environment variables defined in .env
     load_dotenv()
@@ -14,12 +18,14 @@ def main():
     session.login(os.getenv('TIDAL_EMAIL'), os.getenv('TIDAL_PASS'))
     if session.check_login():
         print(f"Successfully logged in as {os.getenv('TIDAL_EMAIL')}")
+        print_hdiv()
 
     # Queries user for valid TIDAL playlist URL
     tidal_url = input('Enter TIDAL playlist URL: ')
     while not(tidal_url.startswith('https://tidal.com/browse/playlist/')):
         print('Invalid TIDAL playlist URL.')
         tidal_url = input('Enter TIDAL playlist URL: ')
+    print_hdiv()
 
 
 if __name__ == '__main__':
