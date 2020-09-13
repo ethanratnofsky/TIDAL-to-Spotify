761df01497750aa42fa28ee1bb62c8c616147ddd
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2020-09-13 09:14:21 -0400

    implemented TIDAL playlist URL input request

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import tidalapi
 
 def main():
     tidal_url = input('Enter TIDAL playlist URL: ')
-    print(tidal_url)
+    while not(tidal_url.startswith('https://tidal.com/browse/playlist/')):
+        print('Invalid TIDAL playlist URL.')
+        tidal_url = input('Enter TIDAL playlist URL: ')
 
 
 if __name__ == '__main__':
